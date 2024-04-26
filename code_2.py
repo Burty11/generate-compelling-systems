d4a3f0f8b9f3dd208abacb007aefa7704277eb56
Burty11 <142454463+Burty11@users.noreply.github.com>
2024-04-26 20:12:18 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,17 @@
+main();
+
+    for (let i = 0; i < 10; i++) {
+        console.log("Random Number: " + item);
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    data.forEach(item => {
+    });
+
+function generateRandomData() {
+}
+    }
+    const data = [];
+    return data;
+    const data = generateRandomData();
+
+}
